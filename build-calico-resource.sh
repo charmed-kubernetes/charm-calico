19ea55e6943abdce29cfc7a04126bc8a8d8631f6
George Kraft <cynerva@gmail.com>
2017-07-26 14:33:25 -0500

    Bump resource versions

--- build-calico-resource.sh
+++ build-calico-resource.sh
@@ -4,9 +4,9 @@ set -eux
 rm -rf resource-build
 mkdir resource-build
 cd resource-build
-wget https://github.com/projectcalico/calicoctl/releases/download/v0.23.1/calicoctl
-wget https://github.com/projectcalico/cni-plugin/releases/download/v1.6.2/calico
-wget https://github.com/projectcalico/cni-plugin/releases/download/v1.6.2/calico-ipam
+wget https://github.com/projectcalico/calicoctl/releases/download/v1.3.0/calicoctl
+wget https://github.com/projectcalico/cni-plugin/releases/download/v1.10.0/calico
+wget https://github.com/projectcalico/cni-plugin/releases/download/v1.10.0/calico-ipam
 chmod +x calicoctl calico calico-ipam
 tar -vcaf ../calico-resource.tar.gz .
 cd ..
