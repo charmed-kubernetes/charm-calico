cdf1b41bce12a9ace51c315cbefd5903809b8be4
George Kraft <cynerva@gmail.com>
2017-08-30 15:05:57 -0500

    Bump calico to v2.5.1

--- build-calico-resource.sh
+++ build-calico-resource.sh
@@ -4,7 +4,7 @@ set -eux
 rm -rf resource-build
 mkdir resource-build
 cd resource-build
-wget https://github.com/projectcalico/calicoctl/releases/download/v1.3.0/calicoctl
+wget https://github.com/projectcalico/calicoctl/releases/download/v1.5.0/calicoctl
 wget https://github.com/projectcalico/cni-plugin/releases/download/v1.10.0/calico
 wget https://github.com/projectcalico/cni-plugin/releases/download/v1.10.0/calico-ipam
 chmod +x calicoctl calico calico-ipam
