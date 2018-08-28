8027a94d859e83e03c6ce9b381c23077926b7446
Kevin W Monroe <kevin.monroe@canonical.com>
2018-08-28 16:48:34 +0000

    build resources for all supported architectures
    The build-calico-resource script is used when pushing/releasing
    new calico charms. When you do that, you'll need to include all
    resources.
    
    Adjust the build script so it creates a resource for each supported
    calico arch (there's nothing arch-specific when making a resource).

--- build-calico-resource.sh
+++ build-calico-resource.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 set -eux
 
-# User can specify which arch resource to build as $1 (default to intel)
-arch=${1:-}
-if [ -z "$arch" ]; then
-  arch="amd64"
-fi
+# Supported calico architectures
+arches="amd64 arm64"
 
 # 2.6.x has no binary releases for arm64; fetch them from neander
 neander="ubuntu@10.96.66.14"
 
-rm -rf resource-build
-mkdir resource-build
-cd resource-build
+fetch_cni_plugins() {
+  arch=${1:-}
+  if [ -z ${arch} ]; then
+    echo "Missing arch parameter to fetch_cni_plugins"
+    exit 1
+  fi
 
-case ${arch} in
-  amd64)
+  mkdir temp
+  (cd temp
+    wget https://github.com/containernetworking/plugins/releases/download/v0.7.1/cni-plugins-${arch}-v0.7.1.tgz
+    tar -vxf cni-plugins-${arch}-v0.7.1.tgz
+    mv portmap ..
+  )
+  rm -rf temp
+}
+
+for arch in ${arches}; do
+  rm -rf resource-build-$arch
+  mkdir resource-build-$arch
+  pushd resource-build-$arch
+
+  if [ $arch = "amd64" ]; then
     wget https://github.com/projectcalico/calicoctl/releases/download/v1.6.4/calicoctl
     wget https://github.com/projectcalico/cni-plugin/releases/download/v1.11.6/calico
     wget https://github.com/projectcalico/cni-plugin/releases/download/v1.11.6/calico-ipam
-    ;;
-  arm64|aarch64)
+  elif [ $arch = "arm64" ]; then
     scp ${neander}:~/go/src/github.com/projectcalico/calicoctl/dist/calicoctl-linux-arm64 ./calicoctl
     scp ${neander}:~/go/src/github.com/projectcalico/cni-plugin/dist/calico ./calico
     scp ${neander}:~/go/src/github.com/projectcalico/cni-plugin/dist/calico-ipam ./calico-ipam
-    ;;
-  *)
-    echo "Unknown arch"
+  else
+    echo "Can't fetch binaries for $arch"
     exit 1
-    ;;
-esac
+  fi
 
-mkdir temp
-(cd temp
-  wget https://github.com/containernetworking/plugins/releases/download/v0.7.1/cni-plugins-${arch}-v0.7.1.tgz
-  tar -vxf cni-plugins-${arch}-v0.7.1.tgz
-  mv portmap ..
-)
-rm -rf temp
+  fetch_cni_plugins $arch
+  chmod +x calicoctl calico calico-ipam portmap
+  tar -vcaf ../calico-$arch.tar.gz .
 
-chmod +x calicoctl calico calico-ipam portmap
-tar -vcaf ../calico-${arch}.tar.gz .
-cd ..
-rm -rf resource-build
+  popd
+  rm -rf resource-build-$arch
+done
