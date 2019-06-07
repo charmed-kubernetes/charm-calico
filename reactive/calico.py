22c26b41d8329237a660a22ec04f4ecc064bd25b
George Kraft <cynerva@gmail.com>
2019-06-07 17:03:47 -0500

    Add global-bgp-peers, subnet-bgp-peers, unit-bgp-peers configs

--- reactive/calico.py
+++ reactive/calico.py
@@ -38,6 +38,7 @@ def upgrade_charm():
     remove_state('calico.npc.deployed')
     remove_state('calico.bgp.globals.configured')
     remove_state('calico.node.configured')
+    remove_state('calico.bgp.peers.configured')
     try:
         log('Deleting /etc/cni/net.d/10-calico.conf')
         os.remove('/etc/cni/net.d/10-calico.conf')
@@ -435,9 +436,83 @@ def reconfigure_node():
     remove_state('calico.node.configured')
 
 
+@when('calico.binaries.installed', 'etcd.available',
+      'leadership.set.calico-v3-data-ready')
+@when_not('calico.bgp.peers.configured')
+def configure_bgp_peers():
+    status_set('maintenance', 'Configuring BGP peers')
+
+    peers = []
+
+    # Global BGP peers
+    config = hookenv.config()
+    peers += yaml.safe_load(config['global-bgp-peers'])
+
+    # Subnet-scoped BGP peers
+    subnet_bgp_peers = yaml.safe_load(config['subnet-bgp-peers'])
+    subnets = filter_local_subnets(subnet_bgp_peers)
+    for subnet in subnets:
+        peers += subnet_bgp_peers[str(subnet)]
+
+    # Unit-scoped BGP peers
+    unit_id = get_unit_id()
+    unit_bgp_peers = yaml.safe_load(config['unit-bgp-peers'])
+    if unit_id in unit_bgp_peers:
+        peers += unit_bgp_peers[unit_id]
+
+    # Give names to peers
+    safe_unit_name = hookenv.local_unit().replace('/', '-')
+    named_peers = {
+        # name must consist of lower case alphanumeric characters, '-' or '.'
+        '%s-%s-%s' % (safe_unit_name, peer['address'], peer['as-number']): peer
+        for peer in peers
+    }
+
+    try:
+        node_name = gethostname()
+        for peer_name, peer in named_peers.items():
+            peer_def = {
+                'apiVersion': 'projectcalico.org/v3',
+                'kind': 'BGPPeer',
+                'metadata': {
+                    'name': peer_name,
+                },
+                'spec': {
+                    'node': node_name,
+                    'peerIP': peer['address'],
+                    'asNumber': peer['as-number']
+                }
+            }
+            calicoctl_apply(peer_def)
+
+        # Delete unrecognized peers
+        existing_peers = calicoctl_get('bgppeers')['items']
+        existing_peers = [peer['metadata']['name'] for peer in existing_peers]
+        peers_to_delete = [
+            peer for peer in existing_peers
+            if peer.startswith(safe_unit_name + '-')
+            and peer not in named_peers
+        ]
+
+        for peer in peers_to_delete:
+            calicoctl('delete', 'bgppeer', peer)
+    except CalledProcessError:
+        log(traceback.format_exc())
+        status_set('waiting', 'Waiting to retry BGP peer configuration')
+        return
+
+    set_state('calico.bgp.peers.configured')
+
+
+@when_any('config.changed.global-bgp-peers', 'config.changed.subnet-bgp-peers',
+          'config.changed.unit-bgp-peers')
+def reconfigure_bgp_peers():
+    remove_state('calico.bgp.peers.configured')
+
+
 @when('calico.service.installed', 'calico.pool.configured',
       'calico.cni.configured', 'calico.bgp.globals.configured',
-      'calico.node.configured')
+      'calico.node.configured', 'calico.bgp.peers.configured')
 @when_any('cni.is-master', 'calico.npc.deployed')
 def ready():
     if not service_running('calico-node'):
@@ -494,18 +569,15 @@ def get_unit_as_number():
     config = hookenv.config()
 
     # Check for matching unit rule
-    unit_id = int(hookenv.local_unit().split('/')[1])
+    unit_id = get_unit_id()
     unit_as_numbers = yaml.safe_load(config['unit-as-numbers'])
     if unit_id in unit_as_numbers:
         as_number = unit_as_numbers[unit_id]
         return as_number
 
     # Check for matching subnet rule
-    ip_address = get_bind_address()
-    ip_address = ipaddress.ip_address(ip_address)  # IP address
     subnet_as_numbers = yaml.safe_load(config['subnet-as-numbers'])
-    subnets = [ipaddress.ip_network(subnet) for subnet in subnet_as_numbers]
-    subnets = [subnet for subnet in subnets if ip_address in subnet]
+    subnets = filter_local_subnets(subnet_as_numbers)
     if subnets:
         subnets.sort(key=lambda subnet: -subnet.prefixlen)
         subnet = subnets[0]
@@ -514,3 +586,15 @@ def get_unit_as_number():
 
     # No AS number specified for this unit.
     return None
+
+
+def filter_local_subnets(subnets):
+    ip_address = get_bind_address()
+    ip_address = ipaddress.ip_address(ip_address)  # IP address
+    subnets = [ipaddress.ip_network(subnet) for subnet in subnets]
+    subnets = [subnet for subnet in subnets if ip_address in subnet]
+    return subnets
+
+
+def get_unit_id():
+    return int(hookenv.local_unit().split('/')[1])
