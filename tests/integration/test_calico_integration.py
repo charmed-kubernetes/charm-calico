1a8e1763a1f76a388fb15bb103f5e6d16e38f563
George Kraft <george.kraft@canonical.com>
2021-06-28 15:18:01 -0500

    Finish test_bgp_service_ip_advertisement

--- tests/integration/test_calico_integration.py
+++ tests/integration/test_calico_integration.py
@@ -1,6 +1,8 @@
 import logging
 import os
 import pytest
+import time
+import yaml
 
 log = logging.getLogger(__name__)
 
@@ -91,22 +93,47 @@ async def test_bgp_service_ip_advertisement(ops_test, kubernetes):
     kubernetes.apply_object(deployment)
     kubernetes.apply_object(service)
     service_ip = kubernetes.read_object(service).spec.cluster_ip
+
     # build and deploy bird charm
     bird_charm = await ops_test.build_charm("tests/data/bird-operator")
     await ops_test.model.deploy(bird_charm)
-    await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
-    # verify test service is not reachable from bird
-    retcode, stdout, stderr = await ops_test.run(
-        'juju', 'ssh', 'bird/leader', 'curl', '--connect-timeout', '3', service_ip
-    )
-    assert retcode == 28, "Failed service connection test before BGP config"
-    # configure bird to peer with calico
+    await ops_test.model.wait_for_idle(wait_for_active=True, timeout=60 * 10)
+
     # configure calico to peer with bird
+    master_config = await ops_test.model.applications['kubernetes-master'].get_config()
+    bird_app = ops_test.model.applications['bird']
+    calico_app = ops_test.model.applications['calico']
+    await calico_app.set_config({
+        'bgp-service-cluster-ips': master_config['service-cidr']['value'],
+        'global-bgp-peers': yaml.dump([
+            {'address': unit.public_address, 'as-number': 64512}
+            for unit in bird_app.units
+        ])
+    })
+
+    # configure bird to peer with calico
+    await bird_app.set_config({
+        'bgp-peers': yaml.dump([
+            {'address': unit.public_address, 'as-number': 64512}
+            for unit in calico_app.units
+        ])
+    })
+
     # verify test service is reachable from bird
-    retcode, stdout, stderr = await ops_test.run(
-        'juju', 'ssh', 'bird/leader', 'curl', '--connect-timeout', '3', service_ip
-    )
-    assert retcode == 0, "Failed service connection test after BGP config"
-    # clean up calico config
-    # remove bird charm
+    deadline = time.time() + 60 * 10
+    while time.time() < deadline:
+        retcode, stdout, stderr = await ops_test.run(
+            'juju', 'ssh', '-m', ops_test.model_full_name, 'bird/leader',
+            'curl', '--connect-timeout', '10', service_ip
+        )
+        if retcode == 0:
+            break
+    else:
+        pytest.fail("Failed service connection test after BGP config")
+
+    # clean up
+    await calico_app.set_config({
+        'bgp-service-cluster-ips': '',
+        'global-bgp-peers': '[]'
+    })
     await bird_app.destroy()
