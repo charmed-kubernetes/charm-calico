0572b1b6c47130e00a5b75d26d74e5050cc10e1f
Adam Dyess <adam.dyess@canonical.com>
2024-06-06 15:09:51 -0500

    Upgrade to calico v3.27.3 (#106)
    * Upgrade to calico v3.27.3
    
    * Update CI file
    
    * Address test issues by installing bird in parallel with the rest of the test stack

--- build-calico-resource.sh
+++ build-calico-resource.sh
@@ -10,7 +10,7 @@ set -eux
 
 # Supported calico architectures
 arches="amd64 arm64"
-calico_version="v3.25.1"
+calico_version="v3.27.3"
 
 function fetch_and_validate() {
   # fetch a binary and make sure it's what we expect (executable > 20MB)
